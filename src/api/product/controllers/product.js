431c624ff5eb2b1f44c491c1fb10a0f910a50337
azharjk <azhr.project@gmail.com>
2022-07-29 06:05:13 +0700

    Format response for findOne products controller

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,33 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::product.product");
+module.exports = createCoreController("api::product.product", () => ({
+  // FIXME: There is unhandle resource not found
+  //        when passing the `id` that not in the database
+  async findOne(ctx) {
+    ctx.query = { populate: "*" };
+
+    const { data: unserializedData } = await super.findOne(ctx);
+
+    const images = [];
+
+    unserializedData.attributes.images.data.forEach((d) => {
+      images.push({
+        url: d.attributes.url,
+      });
+    });
+
+    return {
+      data: {
+        id: unserializedData.id,
+        name: unserializedData.attributes.name,
+        price: unserializedData.attributes.price,
+        condition: unserializedData.attributes.condition,
+        size: unserializedData.attributes.size,
+        minusInfo: unserializedData.attributes.minusInfo,
+        thumbnailUrl: unserializedData.attributes.thumbnailUrl,
+        images,
+      },
+    };
+  },
+}));
