c32769aae4fe89d89944f5f51296b1132b81b688
azharjk <azhr.project@gmail.com>
2022-07-30 19:47:00 +0700

    Format create order response

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,21 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order");
+module.exports = createCoreController("api::order.order", () => ({
+  async create(ctx) {
+    ctx.query = { populate: "product" };
+
+    const { data: unformattedData } = await super.create(ctx);
+
+    return {
+      data: {
+        id: unformattedData.id,
+        name: unformattedData.attributes.name,
+        product: {
+          name: unformattedData.attributes.product.data.attributes.name,
+          price: unformattedData.attributes.product.data.attributes.price,
+        },
+      },
+    };
+  },
+}));
